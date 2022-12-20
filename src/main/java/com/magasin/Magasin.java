93f6cf8963855066dddd27f63afa1c9e879ffa3d
Jorge4 <96417414+Jorge438@users.noreply.github.com>
2022-12-20 15:36:02 +0100

    Refactor magasin V.2

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -7,39 +7,19 @@ class Magasin {
         this.items = items;
     }
 
-    private void updateQuality(Item item){
-        // si l'objet n'est pas un Comté ni un Pass VIP
-        if (!item.name.equals("Comté") && !item.name.equals("Pass VIP Concert")) {
-            increaseOrDecreaseQuality("decrease",item);
-        } else {
-            // sinon on gère le cas du Comté et du Pass VIP
-            increaseOrDecreaseQuality("increase",item);
-
-                // ensuite on gère les régles spécifique au Pass VIP
-                // si l'objet est un Pass Vip
-                if (item.name.equals("Pass VIP Concert")) {
-                    updatePassVIPConcertQuality(item);
-                }
-            }
-        }
-
-    private void updatePassVIPConcertQuality(Item passVIP){
-        // si son SellIn et inférieur à 11
-        if (passVIP.sellIn < 11) {
-            increaseOrDecreaseQuality("increase",passVIP);
-        }
-        // si son SellIn et inférieur à 6
-        if (passVIP.sellIn < 6) {
-            increaseOrDecreaseQuality("increase",passVIP);
+    private void decreaseSellIn(Item item){
+        //si l'objet n'est pas de la Kryptonite alors on diminue le sellIn
+        if (!item.name.equals("Kryptonite")) {
+            item.sellIn = item.sellIn - 1;
         }
     }
 
-    private void increaseOrDecreaseQuality(String action, Item item){
+    private void increaseOrDecreaseQuality(String action, Item item, int nb){
         if(action.equals("increase")){
             //si sa quality est inférieur à 50 et donc peut être augmentée une nouvelle fois
             if (item.quality < 50) {
                 //on augmente la qualité de 1
-                item.quality = item.quality + 1;
+                item.quality = item.quality + nb;
             }
         }
         if(action.equals("decrease")){
@@ -48,62 +28,98 @@ class Magasin {
                 //si l'objet n'est pas de la kryptonite
                 if (!item.name.equals("Kryptonite")) {
                     // la qualité baisse de 1
-                    item.quality = item.quality - 1;
-                }
-                if(item.name.equals("Pouvoirs magiques")){
-                    item.quality = item.quality - 1;
+                    item.quality = item.quality - nb;
                 }
             }
         }
     }
 
-    private void decreaseSellIn(Item item){
-        //si l'objet n'est pas de la Kryptonite alors on diminue le sellIn
-        if (!item.name.equals("Kryptonite")) {
-            item.sellIn = item.sellIn - 1;
+    private void dealComteQuality(Item item){
+        int valueIncreaseComte = 1;
+        if(item.name.equals("Comté")){
+            if(item.sellIn > 0){
+                //La qualité augmente
+                increaseOrDecreaseQuality("increase",item,valueIncreaseComte);
+            } else{
+                //la qualité augmente deux fois plus vite
+                increaseOrDecreaseQuality("increase",item,valueIncreaseComte*2);
+            }
         }
     }
 
-    private void updateSellIn(Item item){
-        decreaseSellIn(item);
-
-        //Si l'objet à un sellIn inférieur à 0
-        if (item.sellIn < 0) {
-
-            //Si l'objet n'est pas un Comté
-            if (!item.name.equals("Comté")) {
-                    //Si l'objet n'est pas un pass VIP
-                    if (!item.name.equals("Pass VIP Concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Kryptonite")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                    }
-                    //Sinon si c'est un Pass VIP la qualité passe à 0
-                    else {
-                            item.quality = item.quality - item.quality;
-                    }
+    private void dealPassVIPQuality(Item item){
+        if(item.name.equals("Pass VIP Concert")){
+            if(item.sellIn>10){
+                increaseOrDecreaseQuality("increase",item,1);
             }
-            //Sinon si c'est un un Comté on augmente la qualité de 1
-            else {
-                    increaseOrDecreaseQuality("increase",item);
+            if (item.sellIn<11 && item.sellIn>5){
+                increaseOrDecreaseQuality("increase",item,2);
             }
+            if (item.sellIn<6 && item.sellIn>0){
+                increaseOrDecreaseQuality("increase",item,3);
+            }
+            if (item.sellIn<1 && item.quality != 0){
+                increaseOrDecreaseQuality("decrease",item, item.quality);
+            }
+        }
+        if(item.quality>50){
+            item.quality = 50;
+        }
+    }
 
+    private void dealPouvoirsMagiquesQuality(Item item){
+        int valueDecreasePouvoirsMagiques = 2;
+            if(item.sellIn > 0){
+                //La qualité augmente
+                increaseOrDecreaseQuality("decrease",item,valueDecreasePouvoirsMagiques);
+            } else{
+                //la qualité augmente deux fois plus vite
+                increaseOrDecreaseQuality("decrease",item,valueDecreasePouvoirsMagiques*2);
+            }
+    }
+
+    private void dealOtherItemQuality(Item item){
+        int valueDecreaseOtherItem = 1;
+        if(item.sellIn > 0){
+            increaseOrDecreaseQuality("decrease",item,valueDecreaseOtherItem);
+        }else {
+            increaseOrDecreaseQuality("decrease",item,valueDecreaseOtherItem*2);
         }
     }
 
     public void updateItems() {
 
         for (int i = 0; i < items.length; i++) {
-
-            // PARTIE QUALITY
-            // PARTIE QUALITY
-            updateQuality(items[i]);
-
-            // PARTIE SELLIN
-            // PARTIE SELLIN
-            updateSellIn(items[i]);
+            switch (items[i].name){
+                case "Comté":
+                    //la qualité du comté est gérée
+                    dealComteQuality(items[i]);
+                    //Le sellIn Baisse
+                    decreaseSellIn(items[i]);
+                    break;
+                case "Pass VIP Concert":
+                    //la qualité du pass est gérée
+                    dealPassVIPQuality(items[i]);
+                    //Le sellIn Baisse
+                    decreaseSellIn(items[i]);
+                    break;
+                case "Kryptonite":
+                    //Le sellIn Baisse
+                    decreaseSellIn(items[i]);
+                    break;
+                case "Pouvoirs magiques":
+                    //la qualité des pouvoirs magiques sont gérées
+                    dealPouvoirsMagiquesQuality(items[i]);
+                    //Le sellIn Baisse
+                    decreaseSellIn(items[i]);
+                    break;
+                default:
+                    //la qualité baisse
+                    dealOtherItemQuality(items[i]);
+                    //Le sellIn Baisse
+                    decreaseSellIn(items[i]);
+                    break;
+            }
         }
     }
 }
